bf0b3d77ed6758555826dc70b8f815c5dcdbf76d
Shade <shadetheartist@gmail.com>
2015-02-11 16:44:54 -0800

    It has begun

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300' rel='stylesheet' type='text/css'>
+        <link href='css/main.css' rel='stylesheet' type='text/css'>
+    </head>
+    
+    <body>
+        <header>
+            <h1 id="title">QUADRILATION</h1>
+            <div id="black-square"></div>
+        </header>
+        
+    </body>
+</html>
